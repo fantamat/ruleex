0ac31188b850e23cd057a98dc3a90b69245d3f0b
fantamat <matlakh@gmail.com>
2019-05-05 00:24:03 +0200

    * update version to synchronyze with gtrain

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='ruleex',
-    version='0.1.1',
+    version='0.3.0',
     description='Rule extraction methods from neural networks',
     long_description=long_description,
     long_description_content_type="text/markdown",
